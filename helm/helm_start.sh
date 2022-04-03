f020da8b6768125df684ad518397f47455ceded0
walter <walter@walter-desktop>
2022-04-03 11:52:24 -0700

    adding helm chart and customized values

--- helm/helm_start.sh
+++ helm/helm_start.sh
@@ -1,4 +1,6 @@
-helm upgrade pinot pinot/pinot \
-    -n pinot-single-node \
-    --set cluster.name=pinot
+helm repo add pinot https://raw.githubusercontent.com/apache/pinot/master/kubernetes/helm
 
+helm upgrade --install pinot pinot/pinot \
+    -n pinot \
+    --set cluster.name=pinot \
+    --values pinot-values.yaml
